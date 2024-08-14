e750d17dbdd94b5ae4abfa1a5a6bec6327016f3e
mazenkaya <58263070+mazenkaya@users.noreply.github.com>
2024-08-14 16:28:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map("map");
+var map = L.map("map").setView([30, 15], 9);
 var tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "Open Street Map"
