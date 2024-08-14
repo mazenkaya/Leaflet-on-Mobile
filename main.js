fc7e8c5223813f148eb49124e15f6d2c20b9db15
mazenkaya <58263070+mazenkaya@users.noreply.github.com>
2024-08-14 16:40:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map("map").setView([51.505, -0.09], 19);
+
 var tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution: "Open Street Map"
+    maxZoom: 19,
+    attribution: "© OpenStreetMap"
 }).addTo(map);
 
 map.locate({
-  setView: true,
-  maxZoom: 19
+    setView: true,
+    maxZoom: 19
 });
 
 function onLocationFound(e) {
-  var radius = e.accuracy;
+    var radius = e.accuracy;
 
-  L.marker(e.latlng).addTo(map)
-   .bindPopup("You are within " + radius + " from this point").openPopup();
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-  L.circle(e.latlng, radius).addTo(map);
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
 };
 
-map.on("locationfound", onLocationFound);
+map.on("locationerror", onLocationError);
